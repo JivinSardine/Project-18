e1ac726000e47a28f1c605da102397848b218e4b
JivinSardine <89859763+JivinSardine@users.noreply.github.com>
2022-01-12 15:52:03 +0530

    update

--- sketch.js
+++ sketch.js
@@ -3,22 +3,23 @@ var PLAY=1;
 var END=0;
 var gameState=1;
 
-var knife,fruit ,monster,fruitGroup,monsterGroup, score,r,randomFruit, position;
-var knifeImage , fruit1, fruit2 ,fruit3,fruit4, monsterImage, gameOverImage;
+var knife,fruit ,monster,fruitGroup,monsterGroup, monsterGroup2, score,r,randomFruit, position, monster2;
+var knifeImage , fruit1, fruit2 ,fruit3,fruit4, monsterImage, gameOverImage, monster2Image;
 var gameOverSound ,knifeSwoosh;
 
 function preload(){
   
   knifeImage = loadImage("knife.png");
-  monsterImage = loadAnimation("alien1.png","alien2.png")
+  monsterImage = loadAnimation("alien1.png","alien2.png");
   fruit1 = loadImage("fruit1.png");
   fruit2 = loadImage("fruit2.png");
   fruit3 = loadImage("fruit3.png");
   fruit4 = loadImage("fruit4.png");
-  gameOverImage = loadImage("gameover.png")
+  gameOverImage = loadImage("gameover.png");
+  monster2Image = loadImage("monster2.png");
   
-  gameOverSound = loadSound("gameover.mp3")
-  knifeSwooshSound = loadSound("knifeSwoosh.mp3")
+  gameOverSound = loadSound("gameover.mp3");
+  knifeSwooshSound = loadSound("knifeSwoosh.mp3");
 }
 
 
@@ -26,6 +27,8 @@ function preload(){
 function setup() {
   createCanvas(600, 600);
   
+  
+
   //creating sword
    knife=createSprite(40,200,20,20);
    knife.addImage(knifeImage);
@@ -40,6 +43,7 @@ function setup() {
   score=0;
   fruitGroup=createGroup();
   monsterGroup=createGroup();
+  monsterGroup2=createGroup();
   
 }
 
@@ -51,6 +55,7 @@ function draw() {
     //Call fruits and Monster function
     fruits();
     Monster();
+    Monster2();
     
     // Move sword with mouse
     knife.y=World.mouseY;
@@ -91,6 +96,23 @@ function draw() {
         knife.x=300;
         knife.y=300;
       }
+
+      if(monsterGroup2.isTouching(knife)){
+        gameState=END;
+        //gameover sound
+        gameOverSound.play()
+        
+        fruitGroup.destroyEach();
+        monsterGroup2.destroyEach();
+        fruitGroup.setVelocityXEach(0);
+        monsterGroup2.setVelocityXEach(0);
+        
+        // Change the animation of sword to gameover and reset its position
+        knife.addImage(gameOverImage);
+        knife.scale=2;
+        knife.x=300;
+        knife.y=300;
+      }
     }
   }
   
@@ -108,8 +130,23 @@ function Monster(){
     monster.y=Math.round(random(100,550));
     monster.velocityX=-(8+(score/10));
     monster.setLifetime=50;
+
     
     monsterGroup.add(monster);
+  }
+}
+
+function Monster2(){
+  if(World.frameCount%300 === 0){
+    monster2=createSprite(400,200,20,20);
+    monster2.addAnimation("moving", monster2Image);
+    monster2.y=Math.round(random(100,550));
+    monster2.velocityX=-(8+(score/10));
+    monster2.setLifetime=50;
+    monster2.scale = 0.1;
+
+    
+    monsterGroup2.add(monster2);
   }
 }
 
